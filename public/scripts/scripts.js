8b2731767294ef2029b39766140f11eece0ace0f
CCLiff <CLCliff30@gmail.com>
2015-01-07 17:36:04 -0500

    configures the navigation to highlight the active location of the user using scrolling height

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -22,16 +22,16 @@ Path.rescue(function(){
 
 $(function(){
 
-  /*--------------------Calculate First Div Height--------------------- */
+/*--------------------Calculate First Div Height--------------------- */
   $('.intro').height($(window).height());
   $('.intro .personal-info').css('top', $(window).height()/2);
-  /*-------------------/Calculate First Div Height--------------------- */
+/*-------------------/Calculate First Div Height--------------------- */
 
-  /*-----------------------Start PathJS Router------------------------- */
+/*-----------------------Start PathJS Router------------------------- */
   Path.listen();
-  /*----------------------/Start PathJS Router------------------------- */
+/*----------------------/Start PathJS Router------------------------- */
 
-  /*------------------------Mobile Navigation-------------------------- */
+/*------------------------Mobile Navigation-------------------------- */
   $('#mob-nav').on('click', function(){
     $('nav').toggleClass('show');
     $('.header h1').toggleClass('menu-open');
@@ -40,16 +40,25 @@ $(function(){
       $(that).children('i').toggleClass('fa-bars fa-arrow-right');
     }, 250);
   });
-  /*-----------------------/Mobile Navigation-------------------------- */
+/*-----------------------/Mobile Navigation-------------------------- */
 
-  /*------------------------Header Stickiness-------------------------- */
-  var vh = $('.intro').height();
+/*-----------------------Scroll Functionality------------------------ */
+
+  
+  var ih = $('.intro').height();
   $(window).on('scroll', function(){
     var fromTop = $('body').scrollTop();
-    $('.about-me .header').toggleClass('top', (fromTop > vh));
-  });
-  /*-----------------------/Header Stickiness-------------------------- */
 
+    /*------------------------Header Stickiness-------------------------- */
+    $('.about-me .header').toggleClass('top', (fromTop > ih));
+    /*-----------------------/Header Stickiness-------------------------- */
+
+    /*-----------------------Active Navigation--------------------------- */
+    $('nav .links ul .home').toggleClass('active', (fromTop < ih - 1));
+    $('nav .links ul #about-me-nav').toggleClass('active', (fromTop > ih - 1));
+    /*-----------------------/Active Navigation-------------------------- */
+
+  });
 
   /*------------------------Scroll to Header--------------------------- */
   $('a[href^="#"').on('click', function(e){
@@ -63,6 +72,8 @@ $(function(){
       }, 750, 'swing');
   });
   /*-----------------------/Scroll to Header--------------------------- */
+
+/*-----------------------/Scroll Functionality----------------------- */
 
 });
 
